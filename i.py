d4ab05bbeb88a8818a01261044c25d3f6448b5ec
Ahmed Barakat <118398763+ahmedbarakat2007@users.noreply.github.com>
2023-06-11 21:43:39 +0300

    Add files via upload

---
+++ i.py
@@ -0,0 +1,78 @@
+from tkinter import *
+import pygame
+import os
+from tkinter import messagebox
+
+class MusicPlayer:
+
+  def __init__(self,root):
+    self.root = root
+    self.root.title("Terrible IPod Emulator")
+    self.root.geometry("350x500")
+    pygame.init()
+    pygame.mixer.init()
+    self.track = StringVar()
+    self.status = StringVar()
+
+    def playsong():
+        try:
+           self.track.set(self.playlist.get(ACTIVE))
+           self.status.set("-Playing")
+           pygame.mixer.music.load(self.playlist.get(ACTIVE))
+           pygame.mixer.music.play()
+           pygame.event.wait()
+        except:
+           msg=messagebox.showerror("ERROR", "Please add Music to Music Folder in ~\Terrible IPod Eimulator\Music")
+           
+
+    def stopsong():
+        try:
+            self.status.set("-Stopped")
+            pygame.mixer.music.stop()
+        except:
+           msg=messagebox.showerror("ERROR", "Opps, There Is an Error While Running :,(")
+
+    def pausesong():
+        try:
+            self.status.set("-Paused")
+            pygame.mixer.music.pause()
+        except:
+           msg=messagebox.showerror("ERROR", "Opps, There Is an Error While Running :,(")
+
+    def unpausesong():
+        try:
+            self.status.set("-Playing")
+            pygame.mixer.music.unpause()
+        except:
+           msg=messagebox.showerror("ERROR", "Opps, There Is an Error While Running :,(")
+
+
+    buttonframe = LabelFrame(self.root,font=("arial",15,"bold"),bg="#5d5e5c",fg="white",relief=GROOVE)
+    buttonframe.place(x=75,y=250,width=200,height=190)
+    playbtn = Button(buttonframe,text="PLAY",command=playsong,width=5,height=2,font=("arial",10,"bold"),fg="white",bg="#5d5e5c").pack(side = TOP)
+    playbtn = Button(buttonframe,text="PAUSE",command=pausesong,width=5,height=2,font=("arial",10,"bold"),fg="white",bg="#5d5e5c").pack(side = LEFT)
+    playbtn = Button(buttonframe,text="UNPAUSE",command=unpausesong,width=5,height=2,font=("arial",10,"bold"),fg="white",bg="#5d5e5c").pack(side = RIGHT)
+    playbtn = Button(buttonframe,text="STOP",command=stopsong,width=5,height=2,font=("arial",10,"bold"),fg="white",bg="#5d5e5c").pack(side = BOTTOM)
+
+    songsframe = LabelFrame(self.root,font=("arial",15,"bold"),bg="white",fg="white",bd=5,relief=GROOVE)
+    songsframe.place(x=75,y=30,width=200,height=200)
+    scroll_y = Scrollbar(songsframe,orient=VERTICAL)
+    self.playlist = Listbox(songsframe,yscrollcommand=scroll_y.set,selectbackground="#B0FC38",selectmode=SINGLE,font=("arial",12,"bold"),bg="#469603",fg="#2b5905",bd=5,relief=GROOVE)
+    scroll_y.pack(side=RIGHT,fill=Y)
+    scroll_y.config(command=self.playlist.yview)
+    self.playlist.pack(fill=BOTH)
+    os.chdir("Music")
+    songtracks = os.listdir()
+    for track in songtracks:
+      self.playlist.insert(END,track)
+    
+    
+
+
+root = Tk()
+MusicPlayer(root)
+
+root.resizable(width=0,height=0)
+root.configure(bg='white')
+root.mainloop()
+
